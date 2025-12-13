d698f6f8ca9560180ecdd616fd072bd3d01db64d
vadmark <random223@mail.ru>
2025-12-14 01:40:51 +0300

    Task in book.

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 )
 
-type Person struct {
-	Name string
-}
+const (
+	workTime = 2 * time.Second
+	waitTime = 1 * time.Second
+)
 
-func changeName(person *Person) {
-	person.Name = "Mark"
+func getText() string {
+	time.Sleep(workTime)
+	return "VADMARK"
 }
 
 func main() {
-	person := Person{"Vad"}
-	fmt.Println(person.Name)
-	changeName(&person)
-	fmt.Println(person.Name)
+	ch := make(chan string, 1)
+	ctx, cancel := context.WithTimeout(context.Background(), waitTime)
+	defer cancel()
+
+	go func(readCh chan<- string) {
+		fmt.Println("Стартуем горутину.")
+		result := getText()
+		select {
+		case readCh <- result:
+			fmt.Println("Получен результат в горутине:", result)
+		case <-ctx.Done(): // Защита от блокировки на отправку, если главный поток уже вышел по cxt.Done()
+			fmt.Println("Контекст выполнился, завершаем горутину.")
+		}
+	}(ch)
+
+	select {
+	case msg := <-ch:
+		fmt.Println("Результат получен в главном потоке:", msg)
+	case <-ctx.Done():
+		fmt.Println("Контекст завершился: ", ctx.Err())
+	}
+
+	fmt.Println("Выходим из главного потока.")
 }
--- internal/trasport/http/handler/bookHandler.go
+++ internal/trasport/http/handler/bookHandler.go
@@ -7,16 +7,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type bookChapter struct {
+	ID   int64
+	Name string
+}
+
 func ShowBook(c *gin.Context) {
 	chapterIDStr := c.Query("chapter_id")
 
+	bookChapters := []bookChapter{
+		{ID: 2, Name: "Срезы (Slices)"},
+		{ID: 9, Name: "Каналы"},
+		{ID: 12, Name: "Defer"},
+	}
+
+	bookTasks := []bookChapter{
+		{ID: -1, Name: "Задача 1"},
+	}
+
 	var (
-		chapterID uint64
+		chapterID int64
 		err       error
 	)
 
 	if chapterIDStr != "" {
-		chapterID, err = strconv.ParseUint(chapterIDStr, 10, 64)
+		chapterID, err = strconv.ParseInt(chapterIDStr, 10, 64)
 		if err != nil {
 			ShowError(c, "Ошибка", err.Error())
 			return
@@ -25,5 +40,7 @@ func ShowBook(c *gin.Context) {
 
 	data := tplWithCapture(c, "Книга по Golang")
 	data["chapter_id"] = chapterID
+	data["chapters"] = bookChapters
+	data["tasks"] = bookTasks
 	c.HTML(http.StatusOK, "book.html", data)
 }
