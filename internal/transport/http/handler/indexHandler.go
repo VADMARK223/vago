40b11b6ac57884b9151a22fb2cf96c8a4256e501
vadmark <random223@mail.ru>
2025-12-22 09:11:55 +0300

    Version 0.8.1 GitHub.

--- internal/transport/http/handler/indexHandler.go
+++ internal/transport/http/handler/indexHandler.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const version = "0.8.0"
+const version = "0.8.1"
 
 func ShowIndex(provider *token.JWTProvider) gin.HandlerFunc {
 	return func(c *gin.Context) {
