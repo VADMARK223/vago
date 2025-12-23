bc48590f59b7cba4c39f9257ae5d8db8282169af
vadmark <random223@mail.ru>
2025-12-23 16:36:23 +0300

    Version 0.9.3: CI/CD: Deploy to VDS

--- internal/transport/http/handler/indexHandler.go
+++ internal/transport/http/handler/indexHandler.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const version = "0.9.2"
+const version = "0.9.3"
 
 func ShowIndex(provider *token.JWTProvider) gin.HandlerFunc {
 	return func(c *gin.Context) {
