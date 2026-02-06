78ed979517dc0e96a8f91f5c6dd874bb5056acd5
vadmark <random223@mail.ru>
2026-02-06 14:58:50 +0300

    Version 1.10.0: Redirect to Go from React.

--- web/v2/dist/assets/ConstObjectAsEnumPattern-BETt4EeS.js
+++ web/v2/dist/assets/ConstObjectAsEnumPattern-BETt4EeS.js
@@ -1,4 +1,4 @@
-import{t as e}from"./CodeBlock-C80_TiuN.js";import{E as t,t as n,u as r}from"./index-C_au5oyt.js";var i=t(),a=r();function o(){return(0,i.useEffect)(()=>{n.admin=`asd`},[]),(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(`a`,{href:`#1`,children:`Почему не enum?`}),(0,a.jsx)(`br`,{}),(0,a.jsx)(e,{code:`export const ROLE = {
+import{t as e}from"./CodeBlock-B2jgU2WP.js";import{E as t,t as n,u as r}from"./index-BrDgW9Ov.js";var i=t(),a=r();function o(){return(0,i.useEffect)(()=>{n.admin=`asd`},[]),(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(`a`,{href:`#1`,children:`Почему не enum?`}),(0,a.jsx)(`br`,{}),(0,a.jsx)(e,{code:`export const ROLE = {
     user: 'user',
     moderator: 'moderator',
     admin: 'admin',
--- web/v2/dist/assets/UtilityTypesChapter-CIfcTuG8.js
+++ web/v2/dist/assets/UtilityTypesChapter-CIfcTuG8.js
@@ -1,4 +1,4 @@
-import{t as e}from"./CodeBlock-C80_TiuN.js";import{u as t}from"./index-C_au5oyt.js";var n=t();function r(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsxs)(`p`,{children:[`Это `,(0,n.jsx)(`b`,{children:`встроенные типы-утилиты`}),`, которые помогают преобразовывать типы.`]}),(0,n.jsx)(e,{code:`type User = {
+import{t as e}from"./CodeBlock-B2jgU2WP.js";import{u as t}from"./index-BrDgW9Ov.js";var n=t();function r(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsxs)(`p`,{children:[`Это `,(0,n.jsx)(`b`,{children:`встроенные типы-утилиты`}),`, которые помогают преобразовывать типы.`]}),(0,n.jsx)(e,{code:`type User = {
 	id: number
 	name: string
 	age: number
