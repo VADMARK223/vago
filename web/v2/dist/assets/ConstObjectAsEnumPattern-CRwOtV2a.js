c5da8826ea76eebc0996ce483165ec914d5b3b26
vadmark <random223@mail.ru>
2026-02-22 13:34:36 +0300

    Version 1.26.2: Universal scroll to top button.

--- web/v2/dist/assets/ConstObjectAsEnumPattern-CRwOtV2a.js
+++ web/v2/dist/assets/ConstObjectAsEnumPattern-CRwOtV2a.js
@@ -1,4 +1,4 @@
-import{g as e,s as t}from"./index-BTwSYiR7.js";import{t as n}from"./Book.module-CETH-CqE.js";import{t as r}from"./BookHashLink-BGaciPBp.js";var i=e();function a(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(`nav`,{className:n.toc,children:(0,i.jsx)(r,{id:`why_not_enum`,children:`Почему не enum?`})}),(0,i.jsx)(t,{code:`export const ROLE = {
+import{_ as e,c as t}from"./index-C7KgGvvy.js";import{t as n}from"./Book.module-CETH-CqE.js";import{t as r}from"./BookHashLink-DWVYUnea.js";var i=e();function a(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(`nav`,{className:n.toc,children:(0,i.jsx)(r,{id:`why_not_enum`,children:`Почему не enum?`})}),(0,i.jsx)(t,{code:`export const ROLE = {
     user: 'user',
     moderator: 'moderator',
     admin: 'admin',
--- web/v2/dist/assets/UtilityTypesChapter-DJhmksc6.js
+++ web/v2/dist/assets/UtilityTypesChapter-DJhmksc6.js
@@ -1,4 +1,4 @@
-import{g as e,s as t}from"./index-BTwSYiR7.js";var n=e();function r(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsxs)(`p`,{children:[`Это `,(0,n.jsx)(`b`,{children:`встроенные типы-утилиты`}),`, которые помогают преобразовывать типы.`]}),(0,n.jsx)(t,{code:`type User = {
+import{_ as e,c as t}from"./index-C7KgGvvy.js";var n=e();function r(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsxs)(`p`,{children:[`Это `,(0,n.jsx)(`b`,{children:`встроенные типы-утилиты`}),`, которые помогают преобразовывать типы.`]}),(0,n.jsx)(t,{code:`type User = {
 	id: number
 	name: string
 	age: number
